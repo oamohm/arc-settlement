0e0cb9122c627494c4d98039a9ca39bb321a49cd
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-17 02:16:42 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,11 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
-const arcTestnet = {
-  id: 5042002,
-  name: 'Arc Testnet',
-  nativeCurrency: { name: 'USDC', symbol: 'USDC', decimals: 18 },
-  rpcUrls: { default: { http: ['https://rpc.testnet.arc.network'] } },
-  blockExplorers: { default: { name: 'ArcScan', url: 'https://testnet.arcscan.app' } },
-  testnet: true
-};
-
-const config = getDefaultConfig({
-  appName: 'Arc Settlement Engine',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [arcTestnet],
-  ssr: true
-});
-
-const queryClient = new QueryClient();
+// ... (बाकी सब वही रखें)
 
 export default function App({ Component, pageProps }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
+        {/* locale="en-US" जोड़ने से पूरा UI इंग्लिश में रहेगा */}
+        <RainbowKitProvider locale="en-US">
           <Component {...pageProps} />
         </RainbowKitProvider>
       </QueryClientProvider>
