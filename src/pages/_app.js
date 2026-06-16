5b16ea5e1ee7a43786aeabd4f413e173935a4bdf
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-17 00:18:29 +0530

    Create _app.js

---
+++ src/pages/_app.js
@@ -0,0 +1,29 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: 'Arc Settlement',
+  projectId: 'YOUR_PROJECT_ID', // आप इसे बाद में बदल सकते हैं
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
