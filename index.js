4465983b144936aafbe919faad8fb547b53b5852
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-16 23:26:18 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>Arc Settlement Engine</h1>
-      <p>System Ready: Status Operational</p>
-    </div>
-  )
-}
