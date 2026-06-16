cff4591dcd048631503cb319af0e86460618e3b0
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-17 01:53:29 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,27 @@
+
+  );
+}
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider, http } from 'wagmi';
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const arcTestnet = {
-  id: 2026,
+  id: 2026, // Arc ID
   name: 'Arc Testnet',
   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: { default: { http: ['https://rpc.testnet.arc.network'] } },
+  blockExplorers: { default: { name: 'ArcScan', url: 'https://testnet.arcscan.app' } },
+  testnet: true, // इसे जोड़ना जरूरी है
 };
 
 const config = getDefaultConfig({
   appName: 'Arc Settlement Engine',
-  projectId: 'YOUR_PROJECT_ID', 
+  projectId: 'YOUR_PROJECT_ID',
   chains: [arcTestnet],
-  transports: { [arcTestnet.id]: http() }, // यहाँ 'http()' का इस्तेमाल करें
+  transports: {
+    [arcTestnet.id]: http('https://rpc.testnet.arc.network')
+  },
   ssr: true,
 });
 
@@ -24,7 +31,7 @@ export default function App({ Component, pageProps }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
+        <RainbowKitProvider modalSize="compact">
           <Component {...pageProps} />
         </RainbowKitProvider>
       </QueryClientProvider>
