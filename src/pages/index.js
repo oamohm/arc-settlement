a5af3e845e298a07d6c9a045f9101460557a0658
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-17 02:24:37 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,3 @@
-import { useAccount, useReadContract } from 'wagmi';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-
 export default function Home() {
-  const { address, isConnected } = useAccount();
-
-  return (
-    <div>
-      <h1>Arc Settlement Engine</h1>
-      <ConnectButton />
-      {isConnected && (
-        <div>
-          <p>Address: {address}</p>
-          <p>Network is connected correctly.</p>
-        </div>
-      )}
-    </div>
-  );
+  return <h1>Hello World</h1>;
 }
