8dab8f179570da3bcb650e64ba2139dca3adebc8
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-16 23:42:51 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Arc Settlement Engine</h1>
+      <p>System Ready: Status Operational</p>
+    </div>
+  )
+}
