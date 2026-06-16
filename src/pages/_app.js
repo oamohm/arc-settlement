52168274c9880cda11e8828d93a79e9e4795c947
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-17 01:24:41 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,37 +1,9 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { WagmiProvider, http } from 'wagmi';
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
+// _app.js के अंदर यह हिस्सा बदलें
 const arcTestnet = {
-  id: 2026,
+  id: 11155111, // अस्थायी रूप से Sepolia ID का उपयोग करें ताकि कनेक्शन बने
   name: 'Arc Testnet',
   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: { default: { http: ['https://rpc.testnet.arc.network'] } },
   blockExplorers: { default: { name: 'ArcScan', url: 'https://testnet.arcscan.app' } },
   testnet: true,
 };
-
-const config = getDefaultConfig({
-  appName: 'Arc Settlement Engine',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [arcTestnet],
-  transports: {
-    [arcTestnet.id]: http('https://rpc.testnet.arc.network'),
-  },
-  ssr: true,
-});
-
-const queryClient = new QueryClient();
-
-export default function App({ Component, pageProps }) {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
-}
