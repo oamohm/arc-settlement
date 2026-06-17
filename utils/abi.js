44c618883caa5e0d3d4e4195caf8c4c53eca5f55
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-18 01:20:49 +0530

    Create abi.js

---
+++ utils/abi.js
@@ -0,0 +1,13 @@
+export const abi = [
+  {
+    "type": "function",
+    "name": "settle",
+    "inputs": [
+      { "name": "_to", "type": "address" },
+      { "name": "_amount", "type": "uint256" }
+    ],
+    "outputs": [],
+    "stateMutability": "payable"
+  }
+];
+
