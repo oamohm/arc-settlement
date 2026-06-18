f8f015131f3a454294d928e151731f31dc3c0d1b
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-18 11:06:56 +0530

    Update abi.js

--- utils/abi.js
+++ utils/abi.js
@@ -1,20 +1,9 @@
 export const abi = [
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "settle",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
+  {
+    "inputs": [{"internalType": "address", "name": "to", "type": "address"}, {"internalType": "uint256", "name": "amount", "type": "uint256"}],
+    "name": "settle",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
 ];
