f762fd0f75577cdb9506ba8ca66044688de85594
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-18 15:48:17 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,32 +1,3 @@
-import { WagmiProvider, createConfig, http } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-// Arc Testnet का सटीक कॉन्फ़िगरेशन
-const arcTestnet = {
-  id: 5042002, 
-  name: 'Arc Testnet',
-  nativeCurrency: { name: 'USDC', symbol: 'USDC', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://rpc.testnet.arc.network'] },
-  },
-  blockExplorers: {
-    default: { name: 'ArcScan', url: 'https://testnet.arcscan.app' },
-  },
-};
-
-const config = createConfig({
-  chains: [arcTestnet],
-  transports: { [arcTestnet.id]: http() },
-});
-
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }) {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+  return <Component {...pageProps} />;
 }
